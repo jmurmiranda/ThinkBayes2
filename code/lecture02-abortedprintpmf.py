a92844c243ee05f73f8aa58def667087d84acb9a
Jose Oscar Mur-Miranda <jmurmiranda@gmail.com>
2014-09-15 12:07:45 -0400

    refined graphics

--- code/lecture02-abortedprintpmf.py
+++ code/lecture02-abortedprintpmf.py
@@ -5,14 +5,24 @@ import thinkplot
 
 d6=thinkbayes2.Pmf(range(1,7))
 three=d6+d6+d6
-three.Incr(1000)
-three.Incr(.5)
-three.Incr(1.5)
+three.Set(1000)
+three.Set(.5)
+three.Set(1.5)
 three.Normalize()
 
 print 'PMF of three dice'
 print three
 
+#==============================================================================
+#     def Print(self):
+#         """Prints the Pmf in a table format.
+# 
+#         returns: Pmf
+#         """
+#         for v, p in self.Items():
+#             print('{val:>8,}  {prob:<}'.format(val=v,prob=p))
+#         return self
+#==============================================================================
 
 #print('{val} {prob}'.format(val='Value',prob='Prob'))
 for v,p in three.Items():
--- code/lecture02.py
+++ code/lecture02.py
@@ -4,27 +4,16 @@ import thinkbayes2
 import thinkplot
 import cmath
 
-#==============================================================================
-#     def Print(self):
-#         """Prints the Pmf in a table format.
-# 
-#         returns: Pmf
-#         """
-#         for v, p in self.Items():
-#             print('{val:>8,}  {prob:<}'.format(val=v,prob=p))
-#         return self
-#==============================================================================
-
-
 d6=thinkbayes2.Pmf(range(1,7))
 three=d6+d6+d6
 print 'PMF of three dice'
 three.Print()
 
-#thinkplot.Hist(d6)
-#thinkplot.Hist(d6+d6)
+thinkplot.Hist(d6)
+thinkplot.Hist(d6+d6)
 thinkplot.Hist(three)
-#thinkplot.Hist(d6+d6+d6+d6+d6)
+thinkplot.Hist(d6+d6+d6+d6+d6)
+thinkplot.Show()
 
 print 'parentheses'
 print('Mean:',three.Mean(),'Var:',three.Var())
@@ -43,12 +32,14 @@ print cmath.sqrt(-1)
 print "\n{0}: {1:,.5f}".format('Pr{three>16}',three.ProbGreater(16))
 print "\n{0}: {1:,.5f}".format('random value from three:',three.Random())
 
-threegiven9=d6+d6+d6
+
+threegiven9=three.Copy()
 for n in range (3,9):
     threegiven9.Set(n,0) #strange... shows values as integers instead of floats
 threegiven9.Normalize()
 threegiven9.Print()
 
-
+thinkplot.Hist(three)
 thinkplot.Hist(threegiven9)
+thinkplot.Show()
 print "\n{0}: {1:,.5f}".format('Pr{threegiven9>16}',threegiven9.ProbGreater(16))
